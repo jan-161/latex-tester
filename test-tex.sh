f1826e88f99ef77fa74af4e1a7fb67165b02bf0c
Jan Haacker <j.m.haacker@tudelft.nl>
2023-09-13 14:55:45 +0200

    Make suitable for multiple tex files

--- test-tex.sh
+++ test-tex.sh
@@ -5,8 +5,15 @@ cp -r . $temp_dir
 cd $temp_dir
 git commit --no-verify -m "temporary testing commit"
 git clean -dqf
-pdflatex main.tex | (! grep -e "Warning: Citation .* undefined")
-if [ "$?" -eq 1 ]; then
-    echo "Update library!"
-    exit 1
-fi
+for tex_file in *.tex
+do
+    grep "^\\\\documentclass{" $tex_file &> /dev/null
+    [ ! "$?" -eq 0 ] && continue
+    echo "Test building file $tex_file..."
+    pdflatex $tex_file | (! grep -e "Warning: Citation .* undefined")
+    if [ "$?" -eq 1 ]; then
+        echo "Update library!"
+        exit 1
+    fi
+    echo "...$tex_file build succeeded."
+done
